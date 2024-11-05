3c453b6bfd02093ee9cac29272393f5beb28a38f
feeshz96 <yofi.pilawan@gmail.com>
2024-11-05 13:20:38 +0700

    ok

--- mobile/javascript/bookmark_config.js
+++
@@ -1,1 +0,0 @@
-﻿var ols=[{caption:"Kalender Akademik 2024 2025 - 1Copaay",page:"1",url:"",level:"1",children:[]},{caption:"Kalender Akademik 2024 2025 - 2Copay",page:"3",url:"",level:"1",children:[]},{caption:"Kalender Akademik 2024 2025 - 3Copy",page:"5",url:"",level:"1",children:[]}];
